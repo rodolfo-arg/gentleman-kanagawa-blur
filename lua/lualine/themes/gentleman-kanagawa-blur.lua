b23b4337335743927f5966eb7c0ba7c41891f97c
Alan Buscaglia <alanbuscaglia@Alans-MacBook-Pro.local>
2025-06-01 11:30:49 +0200

    fix: lualine better contrast

--- lua/lualine/themes/gentleman-kanagawa-blur.lua
+++ lua/lualine/themes/gentleman-kanagawa-blur.lua
@@ -4,15 +4,15 @@ local p = require("gentleman_kanagawa_blur.variants")(variant)
 
 return {
     normal = {
-        a = { fg = p.bg, bg = p.red, gui = "bold" },
+        a = { fg = p.bg_dark, bg = p.blue, gui = "bold" },
         b = { fg = p.fg, bg = p.gray2 },
         c = { fg = p.fg, bg = p.gray1 },
     },
-    command = { a = { fg = p.bg, bg = p.yellow, gui = "bold" } },
-    insert = { a = { fg = p.bg, bg = p.purple, gui = "bold" } },
-    visual = { a = { fg = p.bg, bg = p.magenta, gui = "bold" } },
-    terminal = { a = { fg = p.bg, bg = p.green, gui = "bold" } },
-    replace = { a = { fg = p.bg, bg = p.orange, gui = "bold" } },
+    command = { a = { fg = p.bg_dark, bg = p.accent, gui = "bold" } },
+    insert = { a = { fg = p.bg_dark, bg = p.green, gui = "bold" } },
+    visual = { a = { fg = p.bg_dark, bg = p.magenta, gui = "bold" } },
+    terminal = { a = { fg = p.bg_dark, bg = p.cyan, gui = "bold" } },
+    replace = { a = { fg = p.bg_dark, bg = p.orange, gui = "bold" } },
     inactive = {
         a = { fg = p.gray4, bg = p.bg_dark, gui = "bold" },
         b = { fg = p.gray4, bg = p.bg_dark },
