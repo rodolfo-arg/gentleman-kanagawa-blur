325ad88a6fdd7d7086a5666fee5bb0fc0711d7da
Alan Buscaglia <alanbuscaglia@Alans-MacBook-Pro.local>
2025-06-01 11:39:56 +0200

    fix: naming

--- lua/gentleman_kanagawa_blur/palette.lua
+++ lua/gentleman_kanagawa_blur/palette.lua
@@ -1,4 +1,4 @@
 local config = require("gentleman_kanagawa_blur.config")
 local get_palette = require("gentleman_kanagawa_blur.variants")
 
-return get_palette(config.variant)
+return get_palette(config.variants)
