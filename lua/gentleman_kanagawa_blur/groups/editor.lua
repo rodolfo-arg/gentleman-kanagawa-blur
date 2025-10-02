1b29e277a40743e7a0bda5d7044f37e366afbb59
rodolfo-arg <rodolfo.arguello@rode.com>
2025-10-02 16:26:30 -0700

    chore: change line number color

--- lua/gentleman_kanagawa_blur/groups/editor.lua
+++ lua/gentleman_kanagawa_blur/groups/editor.lua
@@ -41,7 +41,7 @@ return {
 	Question = { fg = p.purple },
 
 	-- Número de línea
-	LineNr = { fg = p.gray5 },
+	LineNr = { fg = p.accent },
 	CursorLineNr = { fg = p.accent }, -- Usamos accent para el número de línea activa
 
 	-- Pmenu (menú de autocompletado) con subtle background
